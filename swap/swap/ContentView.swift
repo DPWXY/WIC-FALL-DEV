959f670954825ba0a48248c62b3df139f9319497
DPWXY <moanawangdp@gmail.com>
2022-11-20 13:00:02 -0800

    test

--- swap/swap/ContentView.swift
+++ swap/swap/ContentView.swift
@@ -11,6 +11,8 @@ struct ContentView: View {
     var body: some View {
         VStack {
             Image(systemName: "globe")
+            Image(systemName: "globe")
+            Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
             Text("Hello, world!")
