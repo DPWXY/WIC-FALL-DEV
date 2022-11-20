6f18e82ad5ec35e5572c0eecbccb7a120b0c16bf
DPWXY <moanawangdp@gmail.com>
2022-11-20 12:44:36 -0800

    init

---
+++ swap/swap/swapApp.swift
@@ -0,0 +1,17 @@
+//
+//  swapApp.swift
+//  swap
+//
+//  Created by Xiaoyue Wang on 11/20/22.
+//
+
+import SwiftUI
+
+@main
+struct swapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
