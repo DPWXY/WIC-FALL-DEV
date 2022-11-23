51dd6d7f192b1df99cc90bfaec6aa3cd91a8440d
sivanivedula <sivani.vedula@gmail.com>
2022-11-22 16:32:11 -0800

    switched to iphone view and basic view no fxns

--- swap/swap/ContentView.swift
+++ swap/swap/ContentView.swift
@@ -10,10 +10,38 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            RoundedRectangle(cornerRadius: 25, style: .continuous)
+                .frame(width: 350, height: 200)
+            
+            HStack {
+                RoundedRectangle(cornerRadius: 25, style: .continuous)
+                    .frame(width: 100, height: 200)
+                RoundedRectangle(cornerRadius: 25, style: .continuous)
+                    .frame(width: 100, height: 200)
+                RoundedRectangle(cornerRadius: 25, style: .continuous)
+                    .frame(width: 100, height: 200)
+                RoundedRectangle(cornerRadius: 25, style: .continuous)
+                    .frame(width: 100, height: 200)
+                
+            }
+            RoundedRectangle(cornerRadius: 25, style: .continuous)
+                .frame(width: 350, height: 200)
+            
+            Spacer()
+            HStack {
+                Image(systemName: "house")
+                Spacer()
+                Image(systemName: "bookmark")
+                Spacer()
+                Image(systemName: "message")
+                Spacer()
+                Image(systemName: "person")
+                Spacer()
+                Image(systemName: "line.3.horizontal")
+                    .imageScale(.large)
+                    .foregroundColor(.accentColor)
+            }
+            
         }
         .padding()
     }
